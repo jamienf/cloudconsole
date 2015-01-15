381b6340cacaad3aef8719771d836c3bf4e66e7f
Jamie Francese <jamienf@gmail.com>
2015-01-14 23:44:27 -0500

    Added ability to edit bands

--- spec/features/bands/user_edits_a_band_spec.rb
+++ spec/features/bands/user_edits_a_band_spec.rb
@@ -6,37 +6,39 @@ I want to edit one of my bands
 So that I can add, remove, or modify its settings
 
 Acceptance Criteria
-[ ] I must be able to edit the band from the unique band page
-[ ] I must be able to update the name, bio, and genre of the band
-[ ] I must be presented with errors if the form is filled out incorrectly
+[X] I must be able to edit the band from the unique band page
+[X] I must be able to update the name, bio, and genre of the band
+[X] I must be presented with errors if the form is filled out incorrectly
 [ ] I must not be able to edit someone else's band.
 ) do
 
+    before :each do
+      @user = FactoryGirl.create(:user)
+      @band1 = FactoryGirl.create(:band)
+      @band2 = FactoryGirl.create(:band)
+    end
+
     scenario "user successfully edits a band" do
-      band = FactoryGirl.create(:band)
+      sign_in_as(@user)
 
-      sign_in_as(band.user)
+      click_link @band1.name
+      expect(page).to have_content @band1.name
+      click_link "Edit Band"
 
-      click_link band.title
-      expect(page).to have_content band.title
-      expect(page).to have_link "Edit Band"
-
-      fill_in "Name", with: "#{band.name}12345"
-      fill_in "Bio", with: "#{band.bio}12345"
-      fill_in "Genre", with: "#{band.genre}12345"
+      fill_in "Name", with: "#{@band1.name}12345"
+      fill_in "Bio", with: "#{@band1.bio}12345"
+      fill_in "Genre", with: "#{@band1.genre}12345"
       click_button "Update Band"
 
-      expect(page).to have_content "#{band.name}12345"
-      expect(page).to have_content "#{band.bio}12345"
-      expect(page).to have_content "#{band.genre}12345"
+      expect(page).to have_content "#{@band1.name}12345"
+      expect(page).to have_content "#{@band1.bio}12345"
+      expect(page).to have_content "#{@band1.genre}12345"
     end
 
     scenario "user fills out bio field incorrectly" do
 
-      playlist = FactoryGirl.create(:band)
-
-      sign_in_as(band.user)
-      visit edit_band_path(band)
+      sign_in_as(@user)
+      visit edit_band_path(@band1)
       fill_in "Bio", with: "x"*301
       click_button "Update Band"
 
@@ -46,7 +48,6 @@ Acceptance Criteria
     end
 
     scenario "user unsuccessfully tries to edit another user's band" do
-
       band1 = FactoryGirl.create(:band)
       band2 = FactoryGirl.create(:band)
 
