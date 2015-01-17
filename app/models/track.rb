0ca95f709acd7b4103602e29ce06e242c0b6fe6a
Jamie Francese <jamienf@gmail.com>
2015-01-16 19:22:57 -0500

    track validations, still need soundcloud validation

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,11 +1,16 @@
 class Track < ActiveRecord::Base
   belongs_to :song
 
+  validates :instrument,
+    presence: true
 
-  def iframe_remover(iframe)
-    html = iframe.gsub!(/"/, '').gsub!(/\s+/, "").gsub!('<iframewidth=70%height=100scrolling=noframeborder=nosrc=', '').gsub!('></iframe>', '')+"&single_active=false"
-    sa = "&single_active=false"
-    final = html + sa
-    final
-  end
+  validates :soundcloud,
+    presence: true
+
+  # def iframe_remover(iframe)
+  #   html = iframe.gsub!(/"/, '').gsub!(/\s+/, "").gsub!('<iframewidth=70%height=100scrolling=noframeborder=nosrc=', '').gsub!('></iframe>', '')+"&single_active=false"
+  #   sa = "&single_active=false"
+  #   final = html + sa
+  #   final
+  # end
 end
