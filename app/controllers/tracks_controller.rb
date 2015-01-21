4a2d5f9fe182ec6a4b89e5609d1e27bdbd32243c
Jamie Francese <jamienf@gmail.com>
2015-01-21 13:09:09 -0500

    fixed sc api validation

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -11,17 +11,22 @@ class TracksController < ApplicationController
     @track.song_id = @song.id
 
     track_url = @track.soundcloud
+    sc_match = /https:\/\/soundcloud.com\/[A-Z0-9._%+-]+\/[A-Z0-9._%+-]+/i
 
-    soundcloud_api = @track.soundcloud_url.get('/resolve', url: track_url)
-  
-    @track.soundcloud = soundcloud_api.id
+    if sc_match.match(track_url)
+      soundcloud_api = @track.soundcloud_url.get('/resolve', url: track_url)
+      @track.soundcloud = soundcloud_api.id
 
-    if @track.save
-      flash[:notice] = "Track successfully saved"
-      redirect_to band_song_path(@band, @song)
+      if @track.save
+        flash[:notice] = "Track successfully saved"
+        redirect_to band_song_path(@band, @song)
+      end
+
     else
+      flash[:notice] = "Please enter a valid Soundcloud URL"
       render :new
     end
+
   end
 
   private
